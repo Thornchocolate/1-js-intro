20515a99557a97de458ae1015cc70ee57f56a589
Thornchocolate <aza.kuzmaite@gmail.com>
2025-03-24 15:10:32 +0200

    ReWrote 5-if.js

---
+++ js/5-if.js
@@ -0,0 +1,27 @@
+// if statement
+let number = 10;
+
+if (number > 5) {
+    console.log("The number is greater than 5");
+}
+
+// if-else statement
+if (number > 15) {
+    console.log("The number is greater than 15");
+} else {
+    console.log("The number is 15 or less");
+}
+
+// if-else if-else statement
+if (number > 15) {
+    console.log("The number is greater than 15");
+} else if (number > 5) {
+    console.log("The number is greater than 5 but 15 or less");
+} else {
+    console.log("The number is 5 or less");
+}
+
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
