e9dddc2fa502089249092d6e305df9cb549e9102
Thornchocolate <aza.kuzmaite@gmail.com>
2025-03-25 09:26:23 +0200

    some tasks

---
+++ task/adult.js
@@ -0,0 +1,16 @@
+/*
+Reikalinga funckija, kuri gauna vartotojo amziu ir grazina ar jis yra pilnametis ar ne.
+amziaus ribos kriterijus: >= 18
+*/
+
+let age = 0
+function isAdult(age) {
+    if (age >= 18) {
+        return `You are an adult.`
+    } else {
+        return `You are not an adult.`
+    }
+}
+console.log(isAdult(18));
+console.log(isAdult(17));
+console.log(isAdult(25));
---
+++ task/textsize.js
@@ -0,0 +1,25 @@
+/* 
+reikaalinga funkcija kuri gauna teksta. kuri grazina rezultata, priklausomai nuo teksto ilgio
+jei tuscias - empty
+jei maziau nei 10, short text from n symbols
+jei maziau nei 20, average text from n symbols
+jei 20 ar daugiau, long text from n symbols
+*/
+
+function textSize(text) {
+    if (text.length == 0) {
+        return "empty";
+    } else if (0 <= text.length && text.length < 20) {
+        return `short text from ${text.length} symbols`;
+    } else if (20 <= text.length && text.length < 40) {
+        return `average text from ${text.length} symbols`;
+    } else {
+        return `long text from ${text.length} symbols`;
+    }
+}
+
+console.log(textSize(''));
+console.log(textSize('Memento mori'));
+console.log(textSize(`The rabbit is white.`))
+console.log(textSize('A white rabbit is running in the forest'));
+console.log(textSize('This morning I woke up, looked through the window and saw a white rabbit hopping around the backyard'));
