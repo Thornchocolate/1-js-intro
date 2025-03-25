d84cd489e46b558170def09f7d07c254ce1708c5
Thornchocolate <aza.kuzmaite@gmail.com>
2025-03-25 11:16:55 +0200

    changes

---
+++ task/marksavg.js
@@ -0,0 +1,30 @@
+
+function marksAverage(marks) {
+    if (!Array.isArray(marks)) {
+        return 'Please provide an array';
+    }
+    if (marks.length === 0) {
+        return 'Please provide at least one mark';
+    }
+    var sum = 0;
+    for (var i = 0; i < marks.length; i++) {
+        if (typeof marks[i] !== 'number' || !isFinite(marks[i]) || 10 < marks[i] < 0) {
+             i++}; 
+             sum += marks[i];
+    }
+             return sum / marks.length;
+    };
+
+
+console.log(marksAverage([5, 7, NaN, 10, 2]));
+console.log(marksAverage([1, 2, 3, 4, 5]));
+console.log(marksAverage([1, 2, 3, 4, 5, 6, 7, 'eight', 9, 10]));
+console.log(marksAverage([]));
+console.log(marksAverage('five'));
+console.log(marksAverage(5));
+console.log(marksAverage(1, 2, 3, 4, 5));
+console.log(marksAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(marksAverage([7, 8, 9, 10]));
+console.log(marksAverage([7, 8, 7, 8]));
+console.log(marksAverage([7, 8, 9, 10]));
+
