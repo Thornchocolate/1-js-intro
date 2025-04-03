5711978d6302a9caa576e33b9e40d02c267e8a54
Thornchocolate <aza.kuzmaite@gmail.com>
2025-04-03 11:16:38 +0300

    array methods

--- js/13-method-array.js
+++ js/13-method-array.js
@@ -17,3 +17,25 @@ for (let i = 0; i < numbers.length; i++) {
     doubleNumbers.push(number * 2);
 }
 console.log(doubleNumbers);
+
+
+const n1 = [1, 11, 111];
+const n2 = [2, 22, 222, 2222];
+const n3 = [3, 33, 333, 3333, 33333];
+
+const n12 = []; 
+
+for (let i = 0; i < n1.length; i++) {
+    const number = n1[i];
+    n12.push(n1[i]);
+}
+for (let i = 0; i < n2.length; i++) {
+    const number = n2[i];
+    n12.push(n2[i]);
+}
+for (let i = 0; i < n3.length; i++) {
+    const number = n3[i];
+    n12.push(n3[i]);
+}
+console.log(n12);
+
